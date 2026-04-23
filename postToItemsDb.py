dae9d968d7f913345568e5e21a3317359f429632
TOX-coding <tmanox04@gmail.com>
2026-04-23 01:56:22 -0500

    TEST_API_KEY

--- postToItemsDb.py
+++ postToItemsDb.py
@@ -4,7 +4,7 @@ from typing import Dict, Any
 
 
 POST_AVAILABILITY_URL = os.getenv("POST_AVAILABILITY_URL")
-AVAILABILITY_API_KEY = os.getenv("AVAILABILITY_API_KEY")
+AVAILABILITY_API_KEY = os.getenv("TEST_API_KEY")
 
 #Yippie
 
